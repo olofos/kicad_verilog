e2edcd2365e59bc7fcd267d76a0b746711798b01
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2024-02-15 23:20:01 +0100

    Basic first draft

---
+++ src/config.rs
@@ -0,0 +1,151 @@
+use anyhow::Result;
+use kicad_netlist::{Component, PinNum, RefDes};
+use nom::{
+    self,
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alphanumeric1, multispace0, none_of},
+    combinator::{map, recognize, value},
+    multi::{many0, many1, separated_list0},
+    sequence::{delimited, preceded, separated_pair, terminated, tuple},
+    IResult,
+};
+
+#[derive(Debug, Clone)]
+pub enum PartPattern {
+    RefDes(String),
+    Part(String),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum PartRule {
+    Skip,
+    External(Vec<String>),
+    Module(String, Vec<String>),
+}
+
+#[derive(Debug, Clone)]
+struct PartPatternRule {
+    pattern: PartPattern,
+    rule: PartRule,
+}
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    part_rules: Vec<PartPatternRule>,
+}
+
+fn part_pattern(i: &str) -> IResult<&str, PartPattern> {
+    alt((
+        map(
+            delimited(tag("["), recognize(many1(none_of("[]= \r\t\n"))), tag("]")),
+            |s: &str| PartPattern::RefDes(s.to_string()),
+        ),
+        map(recognize(many1(none_of("= \r\t\n"))), |s: &str| {
+            PartPattern::Part(s.to_string())
+        }),
+    ))(i)
+}
+
+fn pin_list(i: &str) -> IResult<&str, Vec<String>> {
+    separated_list0(
+        tuple((multispace0, tag(","), multispace0)),
+        map(preceded(tag("#"), alphanumeric1), |s: &str| s.to_owned()),
+    )(i)
+}
+
+fn part_rule(i: &str) -> IResult<&str, PartRule> {
+    alt((
+        value(PartRule::Skip, tag("skip")),
+        map(
+            delimited(tag("module["), pin_list, tag("]")),
+            |pins: Vec<String>| PartRule::External(pins),
+        ),
+        map(
+            tuple((
+                recognize(many1(none_of("()\r\n"))),
+                delimited(tag("("), pin_list, tag(")")),
+            )),
+            |(name, pins): (&str, Vec<String>)| PartRule::Module(name.to_string(), pins),
+        ),
+    ))(i)
+}
+
+fn part_pattern_rule(i: &str) -> IResult<&str, PartPatternRule> {
+    let (i, (pattern, rule)) = terminated(
+        separated_pair(
+            part_pattern,
+            tuple((multispace0, tag("=>"), multispace0)),
+            part_rule,
+        ),
+        multispace0,
+    )(i)?;
+    Ok((i, PartPatternRule { pattern, rule }))
+}
+
+fn part_pattern_rules(i: &str) -> IResult<&str, Vec<PartPatternRule>> {
+    many0(part_pattern_rule)(i)
+}
+
+impl Config {
+    pub fn parse(i: &str) -> Result<Config> {
+        let (_, part_rules) = part_pattern_rules(i).map_err(|err| err.to_owned())?;
+
+        if part_rules
+            .iter()
+            .filter(|pr| matches!(pr.rule, PartRule::External(_)))
+            .count()
+            != 1
+        {
+            anyhow::bail!("Expected exactly one external component");
+        }
+
+        Ok(Config { part_rules })
+    }
+
+    pub fn match_component(&self, comp: &Component) -> Option<&PartRule> {
+        for rule in &self.part_rules {
+            if match &rule.pattern {
+                PartPattern::RefDes(ref_des) => ref_des == comp.ref_des.0,
+                PartPattern::Part(part) => part == comp.part_id.part,
+            } {
+                return Some(&rule.rule);
+            }
+        }
+        None
+    }
+
+    pub fn add_pullup(&mut self, ref_des: RefDes, pin: PinNum) {
+        self.part_rules.push(PartPatternRule {
+            pattern: PartPattern::RefDes(ref_des.0.to_string()),
+            rule: PartRule::Module("pullup".to_string(), vec![pin.0.to_string()]),
+        })
+    }
+
+    pub fn add_pulldown(&mut self, ref_des: RefDes, pin: PinNum) {
+        self.part_rules.push(PartPatternRule {
+            pattern: PartPattern::RefDes(ref_des.0.to_string()),
+            rule: PartRule::Module("pulldown".to_string(), vec![pin.0.to_string()]),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! test_data {
+        ($fname:expr) => {
+            std::fs::read_to_string(concat!(env!("CARGO_MANIFEST_DIR"), "/", $fname)).unwrap()
+        };
+    }
+
+    #[test]
+    fn can_parse_config() {
+        let input = test_data!("alu.vcfg");
+        let config = Config::parse(&input).unwrap();
+        for rule in &config.part_rules {
+            println!("{:?} -> {:?}", rule.pattern, rule.rule);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,248 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+
+use anyhow::{anyhow, Result};
+use config::PartRule;
+use kicad_netlist::{self, NetList, PinType};
+use regex::Regex;
+use std::{
+    borrow::Cow,
+    fs,
+    path::{Path, PathBuf},
+};
+
+use crate::config::Config;
+
+fn make_verilog_name<'a>(name: &'a str) -> Cow<'a, str> {
+    static RE: once_cell::sync::Lazy<Regex> =
+        once_cell::sync::Lazy::new(|| Regex::new(r"^[a-zA-Z_][a-zA-Z0-9$_]*$").unwrap());
+
+    if RE.is_match(name) {
+        name.into()
+    } else {
+        format!(r"\{name} ").into()
+    }
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    let path = if args.len() > 1 { &args[1] } else { "alu.net" };
+    let path = Path::new(path);
+    let input = fs::read_to_string(path)?;
+    let mut netlist: NetList = (&input).try_into()?;
+
+    let module_name = path.file_name().unwrap().to_string_lossy();
+    let module_name = make_verilog_name(module_name.split(".").next().unwrap());
+
+    let path = if args.len() > 2 {
+        PathBuf::from(&args[2])
+    } else {
+        path.with_extension("vcfg")
+    };
+    let input = fs::read_to_string(path)?;
+    let mut config = Config::parse(&input)?;
+
+    let decoupling_caps = netlist
+        .components
+        .iter()
+        .filter_map(|comp| {
+            if comp.part_id.part != "C" {
+                return None;
+            }
+            let nets = comp.pins.iter().map(|pin| pin.net).collect::<Vec<_>>();
+            if nets.len() == 2 && nets.contains(&"VCC") && nets.contains(&"GND") {
+                Some(comp.ref_des)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    netlist.remove_components(&decoupling_caps);
+
+    let pinless_components = netlist
+        .components
+        .iter()
+        .filter_map(|comp| {
+            if comp.pins.is_empty() {
+                Some(comp.ref_des)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    netlist.remove_components(&pinless_components);
+
+    let skipped_components = netlist
+        .components
+        .iter()
+        .filter_map(|comp| {
+            if config.match_component(comp) == Some(&config::PartRule::Skip) {
+                Some(comp.ref_des)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    netlist.remove_components(&skipped_components);
+
+    for comp in &netlist.components {
+        if comp.part_id.part == "R" && comp.pins.len() == 2 {
+            if comp.pins[0].net == "VCC" && comp.pins[1].net != "GND" {
+                config.add_pullup(comp.ref_des, comp.pins[1].num);
+            }
+            if comp.pins[1].net == "VCC" && comp.pins[0].net != "GND" {
+                config.add_pullup(comp.ref_des, comp.pins[0].num);
+            }
+            if comp.pins[0].net == "GND" {
+                config.add_pulldown(comp.ref_des, comp.pins[1].num);
+            }
+            if comp.pins[1].net == "GND" {
+                config.add_pulldown(comp.ref_des, comp.pins[0].num);
+            }
+        }
+    }
+
+    let (comp, pins) = netlist
+        .components
+        .iter_mut()
+        .find_map(|comp| {
+            let rule = config.match_component(comp);
+            match rule {
+                Some(PartRule::External(pins)) => Some((comp, pins)),
+                _ => None,
+            }
+        })
+        .unwrap();
+
+    let nets = pins
+        .iter()
+        .map(|pin_num| {
+            if let Some(pin) = comp.pins.iter().find(|pin| pin.num.0 == pin_num) {
+                Ok(pin.net)
+            } else {
+                Err(anyhow!(
+                    "No pin number {} found for component {}",
+                    pin_num,
+                    comp.ref_des.0
+                ))
+            }
+        })
+        .collect::<Result<Vec<_>>>()?;
+
+    for net_name in &nets {
+        let Some(net) = netlist.nets.iter_mut().find(|net| &net.name == net_name) else {
+            continue;
+        };
+        let all_input = net
+            .nodes
+            .iter()
+            .all(|node| node.ref_des == comp.ref_des || node.typ == PinType::Input);
+        let any_output = net
+            .nodes
+            .iter()
+            .any(|node| node.ref_des != comp.ref_des && node.typ == PinType::Output);
+        let Some(node) = net
+            .nodes
+            .iter_mut()
+            .find(|node| node.ref_des == comp.ref_des)
+        else {
+            continue;
+        };
+        if any_output {
+            node.typ = PinType::Input;
+        }
+        if all_input {
+            node.typ = PinType::Output;
+        }
+        comp.pins
+            .iter_mut()
+            .find(|pin| pin.num == node.num)
+            .expect("There should be a matching pin")
+            .typ = node.typ;
+    }
+
+    let net_string = nets
+        .iter()
+        .map(|net| {
+            let pin = comp.pins.iter().find(|pin| &pin.net == net).unwrap();
+            let name = make_verilog_name(pin.net);
+            let typ = match pin.typ {
+                PinType::Input => "output",
+                PinType::Output => "input",
+                _ => "inout",
+            };
+            format!("{typ} {name}")
+        })
+        .collect::<Vec<_>>()
+        .join(",\n    ");
+
+    println!("module {module_name}\n(\n    {net_string}\n);",);
+
+    let external_nets = nets.into_iter().map(|name| name).collect::<Vec<_>>();
+
+    for net in netlist
+        .nets
+        .iter()
+        .filter(|net| external_nets.contains(&net.name))
+    {
+        eprint!("{}: ", net.name);
+        for node in &net.nodes {
+            eprint!("({}:{},{:?}) ", node.ref_des.0, node.num.0, node.typ);
+        }
+        eprintln!();
+    }
+
+    println!();
+    for net in netlist
+        .nets
+        .iter()
+        .filter(|net| !external_nets.contains(&net.name))
+    {
+        println!("    wire {};", make_verilog_name(net.name));
+    }
+    println!();
+    println!("    assign VCC = 1;");
+    println!("    assign GND = 0;");
+
+    for comp in &netlist.components {
+        if let Some(rule) = config.match_component(comp) {
+            match rule {
+                PartRule::Skip => unreachable!(),
+                PartRule::External(_) => continue,
+                PartRule::Module(name, pins) => {
+                    let nets = pins
+                        .iter()
+                        .map(|pin_num| {
+                            if let Some(pin) = comp.pins.iter().find(|pin| pin.num.0 == pin_num) {
+                                Ok(make_verilog_name(pin.net))
+                            } else {
+                                Err(anyhow!(
+                                    "No pin number {} found for component {}",
+                                    pin_num,
+                                    comp.ref_des.0
+                                ))
+                            }
+                        })
+                        .collect::<Result<Vec<_>>>()?;
+
+                    println!(
+                        "    {} {}({});",
+                        name,
+                        make_verilog_name(comp.ref_des.0),
+                        nets.join(",")
+                    );
+                }
+            }
+        } else {
+            eprintln!(
+                "No rule matching component {}: {}",
+                comp.ref_des.0, comp.part_id.part
+            );
+        }
+    }
+    println!("endmodule");
+
+    Ok(())
 }
